3f294c6b78af866de7d10da13531db1415def7a8
derkrisz <krisztian.dergmail.com>
2017-12-15 16:44:11 +0000

    create guitar instrument subclass

---
+++ src/main/java/Instruments/Guitar.java
@@ -0,0 +1,15 @@
+package Instruments;
+
+public class Guitar extends Instrument {
+
+    private int stringsNumber;
+    private String guitarType;
+    private boolean rightHanded;
+
+    public Guitar(String material, String colour, String brand, double buyPrice, double sellPrice, InstrumentType type, int stringsNumber, String guitarType, boolean rightHanded) {
+        super(material, colour, brand, buyPrice, sellPrice, type);
+        this.stringsNumber = stringsNumber;
+        this.guitarType = guitarType;
+        this.rightHanded = rightHanded;
+    }
+}
