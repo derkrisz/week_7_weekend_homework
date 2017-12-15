cafbc5f24292d12ad65295531e0cc428d7b5ee56
derkrisz <krisztian.dergmail.com>
2017-12-15 16:36:42 +0000

    create instrument class with type enum

---
+++ src/main/java/Instruments/Instrument.java
@@ -0,0 +1,21 @@
+package Instruments;
+
+public abstract class Instrument {
+
+    private String material;
+    private String colour;
+    private String brand;
+    private double buyPrice;
+    private double sellPrice;
+    private InstrumentType type;
+
+    public Instrument(String material, String colour, String brand, double buyPrice, double sellPrice, InstrumentType type) {
+        this.material = material;
+        this.colour = colour;
+        this.brand = brand;
+        this.buyPrice = buyPrice;
+        this.sellPrice = sellPrice;
+        this.type = type;
+    }
+
+}
---
+++ src/main/java/Instruments/InstrumentType.java
@@ -0,0 +1,9 @@
+package Instruments;
+
+public enum InstrumentType {
+    BRASS,
+    STRINGS,
+    WOODWIND,
+    PERCUSSION,
+    KEYBOARD
+}
